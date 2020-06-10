dc6ebbc996d6562565892ad9cc26db5186690089
yaya <1714977322@qq.com>
2020-06-10 19:23:16 +0800

    第一次提交

---
+++ js/login.js
@@ -0,0 +1,32 @@
+$('#login-button').click(function(event) {
+    var userName = document.getElementById("userName").value;
+    var pwd = document.getElementById("pwd").value;
+    //修改密码请改此处
+    if (userName == "袁雅思" && pwd == "19970504") {
+        event.preventDefault();
+        $('form').fadeOut(500);
+        $('.wrapper').addClass('form-success');
+        // requestFullScreen();
+        setTimeout(function() {
+            location.href = "time.html";
+        }, 1000);
+    } else {
+        alert("密码错误，请输入正确密码");
+    }
+});
+
+function requestFullScreen(element) {
+    var element = document.documentElement;
+    var requestMethod = element.requestFullScreen || //W3C
+        element.webkitRequestFullScreen || //Chrome等
+        element.mozRequestFullScreen || //FireFox
+        element.msRequestFullScreen; //IE11
+    if (requestMethod) {
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { //for Internet Explorer
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
